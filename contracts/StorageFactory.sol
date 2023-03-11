3c59401374b4cc89172408d88c5bf10d36e5f109
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-11 20:35:00 +0100

    a

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,11 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+
+pragma solidity ^0.8.7;
+
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // Address
+        // ABI - Application Binary Interface
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    // view means that this function will not modify the state of the contract
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+
+    function sfEdit(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public returns (uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+        return simpleStorage.retrieve();
     }
 }
