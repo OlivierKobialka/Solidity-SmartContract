c1884ba339ca62a9568bbb26f5354a3f6c13ef04
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-11 17:21:19 +0100

    types

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    bool XX = true;
+    uint256 storedData = 1;
+    address owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    string public name = "SimpleStorage";
+    int256 aXX = -1;
+    uint256[] public numbers = [1, 2, 3];
+    bytes32 public hash = 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef;
+}
