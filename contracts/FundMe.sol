5b163e56a801b53d8835c49df667b4d5f7374ec9
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-12 20:08:08 +0100

    withdraw funds | 3 methods

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -17,4 +17,23 @@ contract FundMe {
         funders.push(msg.sender); //? msg.sender is the address of the person who called this function
         addressToAmountFunded[msg.sender] = msg.value;
     }
+
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex]
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);        
+        //! withdraw funds
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Failed to send ether");
+        // call
+        (bool callSucces, /* bytes memory dataReturned */) = payable(msg.sender).call{vale: address(this).balance}("");
+        require(callSucces, "Failed to call ether");
+    }
 }
--- contracts/SafeMathTester.sol
+++ contracts/SafeMathTester.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.8;
 
 contract SafeMathTester {
-    uint8 public bigNumber = 255;
+    uint8 public bigNumber = 255; // checked
 
+    //! resets to 0
     function add() public {
-        bigNumber = bigNumber + 1; //! resets to 0
+        unchecked {
+            bigNumber = bigNumber + 1;
+        }
     }
 }
+
+
+/**
+ * checked keyword is used to check for overflows and underflows
+ * checked and unchecked is making gas cost cheaper 
+ */
