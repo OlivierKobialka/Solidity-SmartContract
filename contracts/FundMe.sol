fb06f40cf1b013961fa8abb1eb9910f9319def6e
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-12 19:26:46 +0100

    conversion rate

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,11 +9,19 @@ contract FundMe {
     function fund() public payable {
         // be able to set the minimum amount of ether to send
         require(msg.value >= minimalUsd, "You need to spend at least 1 ETH");
+        // 18 decimals
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // ABI
         // Address 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419ee
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 1 ETH = 3000.00000000 USD
+        return uint256(price * 1e10); // 1e10 is 10 decimals
     }
 
     function getVersion() view publiv returns (uint256) {
@@ -23,5 +31,11 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    function getConversionRate() public {}
+    function getConversionRate(
+        uint267 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 }
