2efe68d36ec9b8f8eb36366443bcf182e6b2caea
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-29 20:13:41 +0200

    then catch fn.

--- ethers-Simple-Storage/deploy.js
+++ ethers-Simple-Storage/deploy.js
@@ -1,2 +1,15 @@
 // synchronous - [solidity ]
 // Asynchronous - [javascript]
+const ethers = require("ethers");
+// const solc = require("solc")
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {}
+
+main()
+	.then(() => process.exit(0))
+	.catch(error => {
+		console.error(error);
+		process.exit(1);
+	});
