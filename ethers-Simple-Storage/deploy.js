cab624fc47170bdab7972d68bea3f69d59b24387
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-29 20:46:28 +0200

    bin , abi | wallet

--- ethers-Simple-Storage/deploy.js
+++ ethers-Simple-Storage/deploy.js
@@ -6,7 +6,13 @@ const fs = require("fs-extra");
 require("dotenv").config();
 
 async function main() {
-    
+	const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545");
+	const wallet = new ethers.Wallet(
+		"0xc53777392032192d2590ddf272e6932de5a45f38f68eccf703e623b185087a40",
+		provider
+	);
+	const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+	const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
 }
 
 main()
