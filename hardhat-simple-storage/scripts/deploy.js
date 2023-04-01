86a8c9ec9e399a120c8f3d162a7b80be1e2dab19
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-01 13:15:49 +0200

    try catch block

--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -8,10 +8,23 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log(`Deployed contract to: ${simpleStorage.address}`);
+    
 }
 
 async function verify(getContractAddress, args) {
-    
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: getContractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Contract already verified");
+        } else {
+            console.log(error);
+        }
+    }
 }
 
 main()
