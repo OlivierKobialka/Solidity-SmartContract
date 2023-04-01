24db51a26fa8568203b3ccebaa97139dce1866ca
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-01 13:29:45 +0200

    ready to deploy!

--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,5 +1,6 @@
 const { ethers, run, network } = require("hardhat");
 
+// async main
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
         "SimpleStorage"
@@ -8,38 +9,41 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log(`Deployed contract to: ${simpleStorage.address}`);
-    console.log(network.config);
-    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
-        console.log("Waiting for block txes...");
+    // what happens when we deploy to our hardhat network?
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...");
         await simpleStorage.deployTransaction.wait(6);
         await verify(simpleStorage.address, []);
     }
 
     const currentValue = await simpleStorage.retrieve();
-    console.log(`Current value: ${currentValue}`);
+    console.log(`Current Value is: ${currentValue}`);
 
+    // Update the current value
     const transactionResponse = await simpleStorage.store(7);
     await transactionResponse.wait(1);
     const updatedValue = await simpleStorage.retrieve();
-    console.log(`Updated value: ${updatedValue}`);
+    console.log(`Updated Value is: ${updatedValue}`);
 }
 
-async function verify(getContractAddress, args) {
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
     console.log("Verifying contract...");
     try {
         await run("verify:verify", {
-            address: getContractAddress,
+            address: contractAddress,
             constructorArguments: args,
         });
-    } catch (error) {
-        if (error.message.toLowerCase().includes("already verified")) {
-            console.log("Contract already verified");
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
         } else {
-            console.log(error);
+            console.log(e);
         }
     }
-}
+};
 
+// main
 main()
     .then(() => process.exit(0))
     .catch((error) => {
