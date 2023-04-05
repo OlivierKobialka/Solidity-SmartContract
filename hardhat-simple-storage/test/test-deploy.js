182dbffbb997cdb4eaf9ccc43db3cdcc2b0fbcb7
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-05 22:50:21 +0200

    it

--- hardhat-simple-storage/test/test-deploy.js
+++ hardhat-simple-storage/test/test-deploy.js
@@ -3,10 +3,13 @@ const { ethers } = require("hardhat");
 describe("SimpleStorage", function () {
     let simpleStorageFactory, simpleStorage;
     beforeEach(async function () {
-        simpleStorageFunction = await ethers.getContractFactory(
-            "SimpleStorage"
-        );
-        simpleStorage = await simpleStorageFunction.deploy();
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+        simpleStorage = await simpleStorageFactory.deploy();
     });
-    it();
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrive();
+        const expectedValue = "0";
+        // assert || expect key word
+    });
 });
