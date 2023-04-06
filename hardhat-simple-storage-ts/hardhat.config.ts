7a5725c5f8e4bb770bd82f61253e4a8ce0086ff4
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-06 12:47:20 +0200

    little changes

--- hardhat-simple-storage-ts/hardhat.config.ts
+++ hardhat-simple-storage-ts/hardhat.config.ts
@@ -1,8 +1,37 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://eth-goerli/example";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+module.exports = {
+  solidity: "0.8.8",
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
 };
-
-export default config;
--- hardhat-simple-storage-ts/scripts/deploy.ts
+++ hardhat-simple-storage-ts/scripts/deploy.ts
@@ -1,23 +1,50 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
 
+// async main
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+  // what happens when we deploy to our hardhat network?
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is: ${updatedValue}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress: string, args: any[]) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e: any) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
