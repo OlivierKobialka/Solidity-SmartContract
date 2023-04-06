8cb5dac1344829729630c6c9a2476dcabb61b251
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-06 20:01:56 +0200

    async function

---
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -0,0 +1,5 @@
+// hre = Hardhat Runtime Environment
+// module.exports = async (hre) => {
+//     const { deployments, getNamedAccounts } = hre;
+//! }; THE SAME
+module.exports = async ({ getNamedAccounts, deployments }) => {};
