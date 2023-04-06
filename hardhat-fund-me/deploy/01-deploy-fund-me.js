2591e3d5bf3e7b649c3205f1e7c8d5d459f98da6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-06 20:31:55 +0200

    helpers

--- hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -6,4 +6,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
+
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+
+    const fundMe= await deploy("FundMe", {
+        from: deployer,
+        args: [
+            /* address? */
+        ], // put price fred address
+        log: true
+    })
 };
