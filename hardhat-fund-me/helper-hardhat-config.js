0a3d01b18c3604c633a7753a35d3e1e00666163f
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-15 18:12:14 +0200

    z

--- hardhat-fund-me/helper-hardhat-config.js
+++ hardhat-fund-me/helper-hardhat-config.js
@@ -1,18 +1,17 @@
 const networkConfig = {
-    //! chainId 4 = rinkeby
-    4: {
-        name: "rinkeby",
-        ethUsdPriceFeed:
-            "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e" /*price feed address*/,
+    31337: {
+        name: "localhost",
     },
-    137: {
-        name: "polygon",
-        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
     },
-};
+}
 
-const developmentChain = ["hardhat", "localhost"];
-const DECIMALS = 8;
-const INITIAL_ANSWER = 20000000000;
+const developmentChains = ["hardhat", "localhost"]
 
-module.exports = { networkConfig, developmentChain, DECIMALS, INITIAL_ANSWER };
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
